f856c7a96d30f0c9f68a049b2418576289bac92f
Nitesh saini <itsniteshsaini6376@gmail.com>
2025-12-21 19:26:52 +0530

    Time: 0 ms (100%), Space: 29.9 MB (8.52%) - LeetHub

---
+++ 3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+          unordered_set<int> st;
+          int sum = 0, maxNeg = INT_MIN;
+
+          for(int &num: nums){
+            if(num <= 0) maxNeg = max(maxNeg, num);
+            else if(!st.count(num)){
+                sum += num;
+                st.insert(num);
+            }
+          }
+          return sum == 0 ? maxNeg : sum;
+    }
+};
