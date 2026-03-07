9e4e54d101991e0a6a86ec03941693a4d8b1d9aa
Nitesh saini <itsniteshsaini6376@gmail.com>
2026-03-07 22:27:06 +0530

    Time: 42 ms (18.96%), Space: 24 MB (68.38%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,61 +1,47 @@
-#include <vector>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
-    vector<vector<int>> directions{{1, 1}, {0, 1}, {1, 0}, {0, -1}, {-1, 0}, {-1, -1}, {1, -1}, {-1, 1}};
+    typedef pair<int, pair<int, int>> P;
+    vector<vector<int>> directions{{1,1}, {0,1}, {1,0}, {-1, 0}, {0, -1}, {-1, -1}, {1, -1}, {-1, 1}};
 
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        // Edge case: Start or end is blocked
-        if (grid[0][0] != 0 || grid[m - 1][n - 1] != 0) {
+        if (grid[0][0] != 0 || grid[m-1][n-1] != 0)
             return -1;
-        }
-        
-        // Single cell matrix case
-        if (m == 1 && n == 1) return 1;
 
         auto isSafe = [&](int x, int y) {
             return (x >= 0 && x < m && y >= 0 && y < n);
         };
 
-        queue<pair<int, int>> que;
-        que.push({0, 0});
-        grid[0][0] = 1; // Mark as visited by overwriting the grid
+        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
+        priority_queue<P, vector<P>, greater<P>> pq;
+        
+        pq.push({1, {0, 0}});
+        result[0][0] = 1;
 
-        int level = 1; // Start at level 1 because the first cell counts
+        while (!pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
 
-        while (!que.empty()) {
-            int N = que.size();
-            while (N--) {
-                auto curr = que.front();
-                que.pop();
+            int d = curr.first;
+            int x = curr.second.first;
+            int y = curr.second.second;
 
-                int x = curr.first;
-                int y = curr.second;
+            if (x == m - 1 && y == n - 1) 
+                return d;
 
-                // Check all 8 neighbors
-                for (auto& dir : directions) {
-                    int x_ = x + dir[0];
-                    int y_ = y + dir[1];
+            for (auto& dir : directions) {
+                int x_ = x + dir[0];
+                int y_ = y + dir[1];
 
-                    if (isSafe(x_, y_) && grid[x_][y_] == 0) {
-                        // If we reached the bottom-right corner
-                        if (x_ == m - 1 && y_ == n - 1) {
-                            return level + 1;
-                        }
-                        
-                        que.push({x_, y_});
-                        grid[x_][y_] = 1; // Mark as visited
-                    }
+                if (isSafe(x_, y_) && grid[x_][y_] == 0 && d + 1 < result[x_][y_]) {
+                    result[x_][y_] = d + 1;
+                    pq.push({d + 1, {x_, y_}});
                 }
             }
-            level++;
         }
+
         return -1;
     }
 };
